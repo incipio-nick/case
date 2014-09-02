00ea59d8be638b08b1be32f9b1a141a579765e78
Incipio Tech <nkearney@incipio.com>
2014-09-02 12:31:04 -0700

    CLEANED UP STRUCTURE!

---
+++ js/script.js
@@ -0,0 +1,32 @@
+
+	$(window).load(function(){
+		
+
+		//Image parent container is dragable.
+		$('#imgContainer').draggable();
+
+
+		//Image parent container is rotatable.
+		$('#imgContainer').rotatable(); 
+
+	   
+		$('#rotateButton').click( function() { 
+		       $('.ui-rotatable-handle').toggle();
+		        
+		});
+	 	
+
+		//check if check box is checked to allow ratio lock. check!
+		$('#ratioCheck').click( function() {
+		    if($('#ratioCheck').is(":checked"))
+		        ratio =false;
+		    else
+		        ratio = .75;
+		    $(".userImage").resizable('destroy').resizable({
+		        aspectRatio: ratio
+		    });
+		});
+		$(".userImage").resizable({aspectRatio:.75});	
+		 
+
+	}); 
